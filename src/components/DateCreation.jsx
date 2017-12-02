1aa9f7d0a9a299ee14db5621a8f41f8edd212ce0
chi <chulinguy@yahoo.com>
2017-12-02 15:45:14 -0800

    front end done

--- src/components/DateCreation.jsx
+++ src/components/DateCreation.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 
 class DateCreation extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      date
+      date:'',
       time: '',
       location: ''
     }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    
+  }
+
+  componentDidUpdate(){
+    console.log(this.state)
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    axios.post('/api/date/', this.state).then(data =>{
+      if (data.data){
+        window.location="/"
+        alert('Date alert saved!')
+      } else {
+        alert("There was an error, please check your inputs")
+      }
+    })
   }
 
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>
             Date:
             <br/>
@@ -25,6 +44,9 @@ class DateCreation extends React.Component {
             <input id="time" type="time" onChange={e => this.setState({time: e.target.value })} />
             Address:
             <input id="address" type="text" onChange={e => this.setState({location: e.target.value})} />
+            PhoneNumber:
+            <br/>
+            <input id="phoneNumber" type="text" onChange={e => this.setState({phoneNumber: e.target.value})} />
           </label>
           <input type="submit" value="Submit" />
         </form>
