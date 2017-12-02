b7bdb75b526bb8f8053689a1062151e702f7a19f
chulinguy <chulinguy@gmail.com>
2017-12-02 13:18:29 -0800

    updating folder structure

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@ else URL = "http://localhost:3000/auth/facebook/callback"
 
 module.exports = {
 	'facebookAuth' : {
-		'clientID': '',
-		'clientSecret': '',
+		'clientID': '376014406180148',
+		'clientSecret': 'a9b984f6c72b54bf31c2dae019187fd6',
 		'callbackURL': URL
 	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 var webpack = require('webpack');
 var path = require('path');
 var SRC_DIR = path.join(__dirname, '/src');
-var DIST_DIR = path.join(__dirname, '/dist');
+var DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
   entry: `${SRC_DIR}/index.js`,
