f51c0fd6b0a57386e3d495707a2be10a3aff1f2c
Tabitha <tabithahsia@gmail.com>
2018-04-04 22:06:02 -0700

    Logout component

---
+++ src/components/Logout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+class Logout extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+  }
+
+  render() {
+
+  }
+}
+
+export default Logout;
